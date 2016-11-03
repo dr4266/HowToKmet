6cf45b98a86d77530aa33419e979ef25051e4a81
David <dr4266@student.uni-lj.si>
2016-11-03 22:28:13 +0100

    Landing page update - navbar, footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
-	$('#nav-icon').click(function(){
-		$(this).toggleClass('open');
+// Add the open class to the burger menu
+document.addEventListener('DOMContentLoaded', function(event){
+	var navicon = document.getElementById('nav-icon');
+	navicon.addEventListener('click', function(e) {
+		e.preventDefault();
+		navicon.classList.toggle('open');
 	});
 });
+
+// Open/close the navigational menu
+function toggle_navigation() {
+	var nav_div = document.getElementById('navbar-open');
+	if (nav_div.style.width == '100%')
+		nav_div.style.width = '0%';
+	else
+		nav_div.style.width = '100%';
+}
